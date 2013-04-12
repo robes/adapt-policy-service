3e11d22bef15bee1999032349395e9fc6b541a58
robes <robes@users.noreply.github.com>
2013-04-12 06:24:12 +0000

    scaffolding now thread safe

--- adapt/src/adapt/greedy.py
+++ adapt/src/adapt/greedy.py
@@ -5,21 +5,62 @@ Created on Apr 11, 2013
 '''
 import web
 import json
+import threading
+import copy
 
-urls = (
-    '/transfer/(.*)', 'Transfer',
-    '/transfer', 'CreateTransfer'
-)
-app = web.application(urls, globals())
+class Greedy:
+    '''The greedy policy manager.'''
+    
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.transfers = []
+    
+    def all(self):
+        with self.lock:
+            web.debug("all")
+            return copy.deepcopy(self.transfers)
+    
+    def add(self, transfer):
+        with self.lock:
+            web.debug("add: " + str(transfer))
+            transfer["id"] = len(self.transfers)
+            self.transfers.append(transfer)
+            return copy.deepcopy(transfer)
+    
+    def get(self, id):
+        with self.lock:
+            web.debug("get: " + str(id))
+            if id < len(self.transfers):
+                return copy.deepcopy(self.transfers[id])
+            else:
+                return None
+    
+    def update(self, id, transfer):
+        with self.lock:
+            web.debug("update: " + str(id) + ": " + str(transfer))
+            if id < len(self.transfers):
+                self.transfers[id] = transfer
+                return copy.deepcopy(transfer)
+            else:
+                return None
+    
+    def remove(self, id):
+        with self.lock:
+            web.debug("remove: " + str(id))
+            if id < len(self.transfers):
+                self.transfers[id] = None
+
 
 class Transfer:
+        
     def GET(self, transferid):
         if not transferid:
-            transfers = [{"id":123}]
+            transfers = policy.all()
             return json.dumps(transfers)
         
         try:
-            transfer = {'id':int(transferid)}
+            id = int(transferid)
+            transfer = policy.get(id)
             return json.dumps(transfer)
         except ValueError as e:
             msg = "Not a valid transfer id: " + str(e)
@@ -39,10 +80,10 @@ class Transfer:
             web.debug(msg)
             raise web.BadRequest(msg)
         
-        d = web.data()
         try:
+            d = web.data()
             transfer = json.loads(d)
-            web.debug(json.loads(d))
+            policy.update(id, transfer)
         except ValueError as e:
             msg = "Invalid json request body: " + str(e)
             web.debug(msg)
@@ -56,22 +97,33 @@ class Transfer:
         
         try:
             id = int(transferid)
+            policy.remove(id)
         except ValueError as e:
             msg = "Not a valid transfer id: " + str(e)
             web.debug(msg)
             raise web.BadRequest(msg)
-    
+
+
 class CreateTransfer:
     def POST(self):
         d = web.data()
         try:
             transfer = json.loads(d)
-            web.debug(json.loads(d))
-            return d
+            transfer = policy.add(transfer)
+            return json.dumps(transfer)
         except ValueError as e:
             msg = "Invalid json request body: " + str(e)
             web.debug(msg)
             raise web.BadRequest(msg)
+
+
+urls = (
+    '/transfer/(.*)', 'Transfer',
+    '/transfer', 'CreateTransfer'
+)
+policy = Greedy()
+app = web.application(urls, globals())
+
 
 if __name__ == "__main__":
     app.run()
