0a1057820dac1ddc8efde19be4229648e43c0d42
robes <robes@users.noreply.github.com>
2013-04-26 23:05:42 +0000

    Better and cleaner handling of the POST /transfer operation.

--- adapt/test/upd-xfr.sh
+++ adapt/test/upd-xfr.sh
@@ -8,6 +8,6 @@ STREAMS=$2
 
 JSON="{\"id\":\"${ID}\",\"streams\":${STREAMS},\"source\":\"http://localhost:8080/abc\",\"destination\":\"http://localhost:8080/xyz\"}"
 
-curl --request PUT --data ${JSON} --header "${HEADER}" "${BASEURL}/transfer/${ID}"
+curl --request PUT --data "${JSON}" --header "${HEADER}" "${BASEURL}/transfer/${ID}"
 
 echo
