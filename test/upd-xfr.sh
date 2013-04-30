2fba246ac2b1d61248e6d31371a3c8e9940cf421
robes <robes@users.noreply.github.com>
2013-04-30 03:47:45 +0000

    added a delete script, and changed url schemes of src/dst to gsiftp

--- test/upd-xfr.sh
+++ test/upd-xfr.sh
@@ -6,7 +6,7 @@ HEADER="Content-Type: application/json"
 ID=$1
 STREAMS=$2
 
-JSON="{\"id\":\"${ID}\",\"streams\":${STREAMS},\"source\":\"http://localhost:8080/abc\",\"destination\":\"http://localhost:8080/xyz\"}"
+JSON="{\"id\":\"${ID}\",\"streams\":${STREAMS},\"source\":\"gsiftp://localhost:8080/abc\",\"destination\":\"gsiftp://localhost:8080/xyz\"}"
 
 curl --request PUT --data "${JSON}" --header "${HEADER}" "${BASEURL}/transfer/${ID}"
 
