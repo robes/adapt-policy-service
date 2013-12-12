a810eb858363fa7e31ef5889b0da4f3caaf46216
robes <robes@users.noreply.github.com>
2013-12-12 06:41:45 +0000

    1) include LICENSE 2) update setup

--- adapt/setup.py
+++ adapt/setup.py
@@ -1,13 +1,31 @@
-'''
-Created on Apr 11, 2013
+# 
+# Copyright 2013 University of Southern California
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-@author: schuler
-'''
 from distutils.core import setup
-setup(name='policy-adapt',
-      version='0.0',
-      description='Adaptive transfer policy service',
-      packages=['adapt'],
-      package_dir={'': 'src'},
-      scripts=['sbin/policy-service']
+
+setup(name="policy-adapt",
+      version="0.0",
+      description="policy service for file transfer resources",
+      package_dir={"adapt": "src/adapt"},
+      packages=["adapt"],
+      scripts=["sbin/policy-service"],
+      requires=["web.py"],
+      classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        ]
       )
