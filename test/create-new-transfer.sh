c957bd99dc48820e870f8a666dd69e765431b409
robes <robes@users.noreply.github.com>
2013-12-17 04:09:25 +0000

    renamed json body files less cryptically

--- test/create-new-transfer.sh
+++ test/create-new-transfer.sh
@@ -3,6 +3,6 @@
 BASEURL="http://localhost:8080"
 HEADER="Content-Type: application/json"
 
-curl --data @new-xfr.json --header "${HEADER}" "${BASEURL}/transfer"
+curl --data @new-transfer-body.json --header "${HEADER}" "${BASEURL}/transfer"
 
 echo
